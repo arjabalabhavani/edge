1131a3ba1e44464087c58941df21720b14c6a912
Syed Baji Shaik <syedbs@langoor.com>
2021-04-11 22:22:57 +0530

    fix banner

--- js/owlcarousel_js/main.js
+++ js/owlcarousel_js/main.js
@@ -17,5 +17,22 @@ $(document).ready(function () {
             }
         }
     })
+
+    $('.bannerr-carousell').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 });
 
