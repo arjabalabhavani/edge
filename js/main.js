25c4a7de312056437fea0c9e6667bf36e9c92c2c
Syed Baji Shaik <syedbs@langoor.com>
2021-05-10 23:09:51 +0530

    fix

--- js/main.js
+++ js/main.js
@@ -1,60 +1,41 @@
 $(document).ready(function () {
-    // $('.owl-carousel').owlCarousel({
-    //     loop: true,
-    //     margin: 10,
-    //     nav: true,
-    //     dots: true,
-    //     //autoplay: true,
-    //     responsive: {
-    //         0: {
-    //             items: 1
-    //         },
-    //         600: {
-    //             items: 3
-    //         },
-    //         1000: {
-    //             items: 1
-    //         }
-    //     }
-    // })
-
     $('.bannerr-carousell').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     })
     $('.services-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        dots:true,
-        autoplay:true,
-        autoplayTimeout:2000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1
+        loop: false,
+        margin: 0,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:2
+            1000: {
+                items: 2
             }
         }
     })
-}); 
+});
 
 
 
